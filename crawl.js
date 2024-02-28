14996b4cc109ceccbd7a0ee7da93b9b7329569d6
JulianKerns <j.kernstock@gmx.at>
2024-02-28 18:45:45 +0100

    written a function to normalize URLs and a function that gets all the URLs from a HTML body

--- crawl.js
+++ crawl.js
@@ -1,15 +1,51 @@
 function normalizeURL(url){
     try{
     const adress = new URL(url)
-    return adress.origin +'/path'
-
+    let fullPath = `${adress.hostname}${adress.pathname}`
+    if(fullPath.length > 0 && fullPath.slice(-1) === '/'){
+         fullPath = fullPath.slice(0,-1)
+    }
+    return fullPath
     }catch(TypeError){
-    return 'Not a valid URL'
+        return 'Not a valid URL'
     }
 }
+
+function getURLsfromHTML(htmlBody, baseUrl){
+    const {JSDOM} = require('jsdom')
+    const dom = new JSDOM(htmlBody)
+    const linkList = []
+    const webpageUrl = dom.window.document.querySelectorAll("a")
+    for(const item of webpageUrl){
+        let url = item.getAttribute('href')
+        if(url[0] === '/'){
+            try{
+            url = baseUrl + url
+            let urlvalid = new URL(url).href
+            linkList.push(url) 
+        }catch(err){
+            console.log(`${err.message} : ${url}`)
+        }
+        }else{
+            try{
+                let urlvalid = new URL(url).href
+                linkList.push(url)
+            }catch(err){
+                console.log(`${err.message} : ${url}`)
+            }
+           
+        }
+        
+       
+    }
+    return linkList
+    }
+   
+
 
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,28 +1,29 @@
 const {test, expect} = require('@jest/globals')
 const {normalizeURL} = require('./crawl.js')
+const {getURLsfromHTML} = require('./crawl.js')
 
-test('https://blog.boot.dev/clean-code/give-up-sooner/ should be converted to https://blog.boot.dev/path ', () => {
-    expect(normalizeURL('https://blog.boot.dev/clean-code/give-up-sooner/')).toBe('https://blog.boot.dev/path');
+test('protocol test  ', () => {
+    expect(normalizeURL('https://blog.boot.dev/clean-code/give-up-sooner/')).toBe('blog.boot.dev/clean-code/give-up-sooner');
 });
 
-test('https://blog.boot.dev/news/bootdev-beat-2024-02/ should be converted to https://blog.boot.dev/path', () =>{
-    expect(normalizeURL('https://blog.boot.dev/news/bootdev-beat-2024-02/')).toBe('https://blog.boot.dev/path');
+test('http test ', () =>{
+    expect(normalizeURL('https://blog.boot.dev/news/bootdev-beat-2024-02/')).toBe('blog.boot.dev/news/bootdev-beat-2024-02');
 });
 
-test('https://blog.boot.dev/computer-science/ai-taking-programming-jobs/ should be converted to https://blog.boot.dev/path', () => {
-    expect(normalizeURL('https://blog.boot.dev/computer-science/ai-taking-programming-jobs/')).toBe('https://blog.boot.dev/path');
+test('Capital test ', () => {
+    expect(normalizeURL('https://BLOG.boot.dev/computer-science/ai-taking-programming-jobs/')).toBe('blog.boot.dev/computer-science/ai-taking-programming-jobs');
 });
 
-test('https://blog.boot.dev/clean-code/youre-not-qualified-for-tech-opinions/ should be converted to https://blog.boot.dev/path', () => {
-    expect(normalizeURL('https://blog.boot.dev/clean-code/youre-not-qualified-for-tech-opinions/')).toBe('https://blog.boot.dev/path');
+test('https://blog.boot.dev/clean-code/youre-not-qualified-for-tech-opinions/ should be converted to blog.boot.dev/path', () => {
+    expect(normalizeURL('https://blog.boot.dev/clean-code/youre-not-qualified-for-tech-opinions/')).toBe('blog.boot.dev/clean-code/youre-not-qualified-for-tech-opinions');
 });
 
-test('https://www.wowhead.com/guide/classes/monk/windwalker/bis-gear should be converted to https://www.wowhead.com/path', () => {
-    expect(normalizeURL('https://www.wowhead.com/guide/classes/monk/windwalker/bis-gear')).toBe('https://www.wowhead.com/path')
+test('https://www.wowhead.com/guide/classes/monk/windwalker/bis-gear should be converted towww.wowhead.com/path', () => {
+    expect(normalizeURL('https://www.wowhead.com/guide/classes/monk/windwalker/bis-gear')).toBe('www.wowhead.com/guide/classes/monk/windwalker/bis-gear')
 })
 
-test('https://www.wowhead.com/guides/classes should be converted to https://www.wowhead.com/path', () => {
-    expect(normalizeURL('https://www.wowhead.com/guides/classes')).toBe('https://www.wowhead.com/path')
+test('https://www.wowhead.com/guides/classes should be converted to www.wowhead.com/path', () => {
+    expect(normalizeURL('https://www.wowhead.com/guides/classes')).toBe('www.wowhead.com/guides/classes')
 })
 
 test('string is not a valid URL, returns Not a valid URL', () => {
@@ -34,11 +35,57 @@ test('string is not a valid URL, returns Not a valid URL', () => {
 test('string is not a valid URL, returns Not a valid URL', () => {
     expect(normalizeURL('wowhead.com')).toBe('Not a valid URL')
 })
-
-test('Url normalized to its origin', ()=> {
-    expect(normalizeURL('https://www.google.com/search?q=package-lock.json&oq=package-lock.json&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIGCAcQRRg8qAIAsAIA&sourceid=chrome&ie=UTF-8')).toBe('https://www.google.com/path')
+test('https://en.wikipedia.org/wiki/Edge_case should return en.wikipedia.org/path', ()=> {
+    expect(normalizeURL('https://en.wikipedia.org/wiki/Edge_case')).toBe('en.wikipedia.org/wiki/Edge_case')
 })
 
-test('https://en.wikipedia.org/wiki/Edge_case should return https://en.wikipedia.org/path', ()=> {
-    expect(normalizeURL('https://en.wikipedia.org/wiki/Edge_case')).toBe('https://en.wikipedia.org/path')
+test('https://en.wikipedia.org/wiki/Edge_case should return en.wikipedia.org/path', ()=> {
+    expect(normalizeURL('https://en.wikipedia.org/wiki/Edge_case')).toBe('en.wikipedia.org/wiki/Edge_case')
+})
+
+test('getUrlsfromHTML basic test', () => {
+    const input1 = `<html>
+    <body>
+        <a href="https://blog.boot.dev"></a>
+        <a href="https://blog.boot.dev/clean-code/give-up-sooner"></a>
+    </body>
+</html>
+` 
+    const input2 = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input1,input2)
+    const expected = ['https://blog.boot.dev','https://blog.boot.dev/clean-code/give-up-sooner']
+    expect(actual).toEqual(expected)
+})
+
+
+test(' getUrlsfromHTML - relative to absolut URLs', () => {
+    const input1 = `<html>
+    <body>
+        <a href="/backend"></a>
+        <a href='https://blog.boot.dev/clean-code/give-up-sooner'></a>
+        <a href="/news/bootdev-beat-2024-02/"></a>
+       
+    </body>
+</html>
+` 
+    const input2 = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input1,input2)
+    const expected = ['https://blog.boot.dev/backend','https://blog.boot.dev/clean-code/give-up-sooner','https://blog.boot.dev/news/bootdev-beat-2024-02/']
+    expect(actual).toEqual(expected)
+})
+
+test(' getUrlsfromHTML - relative to absolut URLs', () => {
+    const input1 = `<html>
+    <body>
+        <a href="invalid"></a>
+        <a href='https://blog.boot.dev/clean-code/give-up-sooner'></a>
+        <a href="/news/bootdev-beat-2024-02/"></a>
+       
+    </body>
+</html>
+` 
+    const input2 = 'https://blog.boot.dev'
+    const actual = getURLsfromHTML(input1,input2)
+    const expected = ['https://blog.boot.dev/clean-code/give-up-sooner','https://blog.boot.dev/news/bootdev-beat-2024-02/']
+    expect(actual).toEqual(expected)
 })
