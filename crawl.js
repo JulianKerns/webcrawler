58bcaaf8a5a62977c5f03ce4f3857d9c04aa5476
JulianKerns <j.kernstock@gmx.at>
2024-02-29 13:53:33 +0100

    Created a main function that utilizes a CLI argument and implemented a fetch call to retrive the HTML body of a Webpage

--- crawl.js
+++ crawl.js
@@ -39,9 +39,38 @@ function getURLsfromHTML(htmlBody, baseUrl){
     }
     return linkList
     }
-   
+
+async function crawlPage(currUrl){
+    let response = null
+    try{
+     response = await fetch(currUrl,{
+        method : 'GET',
+        mode : 'cors',
+        
+    })}
+    catch(err){
+        return `An Error occured during fetching ${err.message}`
+    } 
+    
+    try{
+    if(response.status < 400 && response.headers.get('content-type').includes('text/html')){
+        const HTMLbody = await response.text()
+        console.log(`${HTMLbody}`)
+    }
+    else{
+        console.log('Error: Could not retrive Server-Data')
+    }
+    }catch(err){
+        console.log(`Error occured: ${err.message}`)
+    }
+
+}
+
+
+
 
 module.exports = {
     normalizeURL,
-    getURLsfromHTML
+    getURLsfromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+const{crawlPage} = require('./crawl.js')
+
+function main(){
+    const {argv} = require('node:process')
+    if(argv.length < 3 ){
+        console.log('Error: No processable arguments given')
+    }else if( argv.length > 3){
+        console.log('Error: To many processable arguments given')
+
+    }else{
+        console.log(`Commence crawling the website at its root: ${argv[2]}`)
+        const crawl = crawlPage(argv[2])
+        return crawl
+    }
+
+}
+main()
